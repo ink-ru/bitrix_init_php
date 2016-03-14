6764f47c7a3cc75255eb7df191aa9d8575609993
ink-ru <george.a.wise@gmail.com>
2016-03-14 13:38:05 +0300

    Update init.php

--- init.php
+++ init.php
@@ -30,4 +30,65 @@ function d($ar, $userid = 0)
             echo '<pre>NULL</pre>';
     } 
 }
+
+
+/**
+* Фунция для просмотра и отладки данных на сайте
+* $ar - параметр, принимающий любой тип данных для просмотра его содержания
+* $userid - id пользователя в системе
+* Пример: d($arr, 1);
+* (дамп виден только под админом)
+* 
+* @param mixed $content
+*/
+
+//============= Закрываем индексацию страниц ===========//
+// uncomment next line to init
+// AddEventHandler("main", "OnEndBufferContent", "GoogleBotDirective");
+
+function GoogleBotDirective(&$content)
+{
+    $path = $_SERVER["DOCUMENT_ROOT"].'robots.txt';
+    $noindex = '<meta name="googlebot" content="noindex">';
+    $buffered = false;
+    
+    if(@file_exists($path)) $sR = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        else $sR = file('http://'.$_SERVER['SERVER_NAME'].'/robots.txt');
+    $sR = array_unique(array_filter($sR));
+    
+    foreach($sR as $sRule) 
+    {
+        if(strpos($sRule, '#')) $sRule = preg_replace('~^([^#]*)#~i', "$1", $sRule); // comment removing
+        
+        if(!preg_match('~Disallow.*~', $sRule)) continue; // нам нужны только запрещающие правила
+        
+        if($content !== false)
+        {
+          if(!preg_match('~<meta[^>]+name\s*=\s*["|\']googlebot["|\'][^>]+noindex[^>]+>~siU', $content)) $buffered = true;
+            else continue;
+        }
+        $sRepFrom = array( '*',  '?' );
+        $sRepTo =   array( '.*', '\?');
+        $sRule = preg_replace('~^\s*Disallow\s*:\s*~i', '', $sRule);
+        $sRule = trim(str_replace($sRepFrom,$sRepTo,$sRule));
+        if(!strpos($sRule,'$')) // на всякий случай
+        {
+          $sRule = preg_replace('~([^\*])$~i', "$1.*", $sRule);
+        }
+        
+        
+        if(preg_match('~'.$sRule.'~i', $_SERVER['REQUEST_URI']))
+        {
+            if($buffered) $content = str_ireplace('</title>', '</title>'.$noindex, $content);
+              else print $noindex;
+            
+            return true;
+            break;
+        }
+        
+    }
+    
+    return true;
+}
+
 ?>
